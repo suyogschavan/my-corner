a912b6d8fbcf8fa6c90d26b2882e2f805e977904
Suyog <suyogschavan03@gmail.com>
2024-06-28 00:12:47 +0530

    "Updated globals.css, layout.tsx, page.tsx, next.config.mjs, package-lock.json, and package.json files; added new dependencies and plugins; modified tailwind.config.ts"

---
+++ app/projects/page.tsx
@@ -0,0 +1,12 @@
+import ProjectParalax from "@/components/ProjectParalax";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <ProjectParalax />
+    </div>
+  );
+};
+
+export default page;
