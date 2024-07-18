b92b79f616e55ac9aa4b8bf7095135d70596520c
Suyog <suyogschavan03@gmail.com>
2024-07-18 16:31:37 +0530

    "Moved EEG Emotion Classification project to a different position in the projects array."

--- data/project.tsx
+++ data/project.tsx
@@ -6,16 +6,16 @@ export const projects = [
       "https://github.com/suyogschavan/jobLister/blob/main/public/JobLister.png?raw=true",
   },
   {
-    title: "EEG Emotion Classification",
-    link: "https://github.com/suyogschavan/Caps-Project-Emotion-Classification-using-EEG",
-    thumbnail:
-      "https://github.com/suyogschavan/Caps-Project-Emotion-Classification-using-EEG/blob/main/Outputs/ICA_components.png?raw=true",
-  },
-  {
     title: "Portfolio website #1",
     link: "https://github.com/suyogschavan/suyogschavan.github.io/",
     thumbnail:
       "https://github.com/suyogschavan/suyogschavan.github.io/blob/main/assets/img/port.png?raw=true",
+  },
+  {
+    title: "EEG Emotion Classification",
+    link: "https://github.com/suyogschavan/Caps-Project-Emotion-Classification-using-EEG",
+    thumbnail:
+      "https://github.com/suyogschavan/Caps-Project-Emotion-Classification-using-EEG/blob/main/Outputs/ICA_components.png?raw=true",
   },
   // {
   //   title: "E-commerce Backend",
